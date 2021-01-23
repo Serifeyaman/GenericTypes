26e8f2235e0af8314a6e10f1de66fa799ba5c0ca
Şerife <eyaman895@gmail.com>
2021-01-23 20:11:53 +0300

    Proje dosyası ekle.

---
+++ GenericTypes/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericTypes
+{
+    class MyDictionary<Key_T, Value_T>
+    {
+        Key_T[] _arrayKey;          // key değerlerini tutacak bir dizi oluşturdum
+        Value_T[] _arrayValue;      // value değerlerini tutacak bir dizi oluşturdum
+        public MyDictionary()
+        {
+            _arrayKey = new Key_T[0];                                   //keys için 0 elemanlı boş dizi oluşturulur
+            _arrayValue = new Value_T[0];                               //values için
+        }
+
+        //MyDictionary için eleman ekleme metodu
+        public void Add(Key_T anahtar, Value_T deger)                   //gelen key ve value değerlerini eklemek için fonksiyon yazılır
+        {
+            Key_T[] tempKeyArray = _arrayKey;                           //eklenen ilk key leri tutmak için geçici bir dizi oluşturulur
+            Value_T[] tempValueArray = _arrayValue;
+
+            _arrayKey = new Key_T[_arrayKey.Length + 1];                //yeni key alabilmek için key dizisinin boyutunun bir fazlası boyutta dizi oluşturulur
+            _arrayValue = new Value_T[_arrayValue.Length + 1];
+
+            //temp deki elemanlar alınır
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                _arrayKey[i] = tempKeyArray[i];
+                _arrayValue[i] = tempValueArray[i];
+            }
+            _arrayKey[_arrayKey.Length - 1] = anahtar;                  // yeni eklenen key, key dizisinin sonuna eklenir
+            _arrayValue[_arrayValue.Length - 1] = deger;
+        }
+
+        //MyDictionary eleman sayısını verir
+        public int Count
+        {
+            get { return _arrayKey.Length; }
+        }
+
+        //MyDictionary eleman listeleme (NOT!! Sadece elemanları eklediğini görmek için yazılmıştır)
+        public void Key_Value()
+        {
+            for (int i = 0; i < _arrayKey.Length; i++)
+            {
+                Console.WriteLine(_arrayKey[i] + "     ---->    " + _arrayValue[i]);
+            }
+        }
+
+    }
+}
---
+++ GenericTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary Sınıfı");
+
+            Dictionary<int, string> sehirler = new Dictionary<int, string>();             
+            Console.WriteLine("Eleman Sayısı: " + sehirler.Count);   // eleman yokken eleman sayısı 0 olacak
+
+            sehirler.Add(58, "Sivas");
+            sehirler.Add(38, "Kayseri");
+            sehirler.Add(06, "Ankara");
+
+            Console.WriteLine("Eleman Sayısı: " + sehirler.Count);        //eleman eklendikten sonraki sayısı
+
+            Console.WriteLine("Plakalar      Şehirler");
+            Console.WriteLine("---------------------------");
+            foreach (var sehir in sehirler)
+            {
+                Console.WriteLine(sehir.Key + "     ---->    " + sehir.Value);
+            }
+
+            Console.WriteLine("***************************");
+            Console.WriteLine("MyDictionary Sınıfı");
+            //Benim oluşturduğum dictionary 
+            MyDictionary<int, string> myDict = new GenericTypes.MyDictionary<int,string>();
+
+            Console.WriteLine("Eleman Sayısı: " + myDict.Count);        //eleman eklenmeden önceki sayısı
+
+            myDict.Add(25, "Erzurum");              //eleman ekleme
+            myDict.Add(58, "Sivas");
+            myDict.Add(01, "Adana");
+
+            Console.WriteLine("Eleman Sayısı: " + myDict.Count);        //eleman eklendikten sonraki sayısı
+
+            Console.WriteLine("Plakalar      Şehirler");
+            Console.WriteLine("---------------------------");
+   
+            myDict.Key_Value();                     //elemanları listeleme
+        }
+    }
+}
